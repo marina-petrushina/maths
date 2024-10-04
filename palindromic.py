facaa685967e7ab19267662209c5a0e510629953
Marina Petrushina <m-petrushina@yandex-team.ru>
2024-10-04 14:28:25 +0300

    delete files

--- palindromic.py
+++
@@ -1,9 +0,0 @@
-x = 0
-for i in range(100, 1000):
-    for j in range(100,1000):
-        a1 = str(i*j)
-        a2 = a1[::-1]
-        if a2 == a1 and int(a1) > x:
-            x = int(a1)
-print(x)
-        
